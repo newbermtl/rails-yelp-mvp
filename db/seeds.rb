d9e0d28a5ae1851ec130049e5f110101e839dc67
Frederic Lauzier <lauzier.frederic@gmail.com>
2020-11-11 11:59:20 -0500

    database seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create([{ name: 'Pho Lien', address: 'Cote des Neiges', phone_number: '450-298-9848', category: 'chinese' },
+                   { name: 'Bombay Mahal', address: 'Plateau', phone_number: '450-298-9848', category: 'french' },
+                   { name: 'Kyo', address: 'Vieux-port', phone_number: '450-298-9848', category: 'japanese' },
+                   { name: 'Frite Alors', address: 'Laurier', phone_number: '450-298-9848', category: 'belgian' },
+                   { name: 'Stela Pizzeria', address: 'Laurier', phone_number: '450-298-9848', category: 'italian' }])
