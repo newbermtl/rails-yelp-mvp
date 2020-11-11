e8982998fa3fed80b2325d6d8df957d1c42f70e8
Frederic Lauzier <lauzier.frederic@gmail.com>
2020-11-11 13:34:47 -0500

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# Restaurant: index, create(new), show(with review)
+# Review: create(new)
+
+  resources :restaurants, only: %i[new create index show] do
+    resources :reviews, only: %i[create new]
+  end
 end
