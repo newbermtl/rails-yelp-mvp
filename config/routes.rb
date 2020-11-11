2c82b6416c94033f42330080d700a27cd0764a3b
Frederic Lauzier <lauzier.frederic@gmail.com>
2020-11-11 15:49:17 -0500

    Controllers and views working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'reviews/create'
+  get 'reviews/new'
+  get 'restaurants/index'
+  get 'restaurants/create'
+  get 'restaurants/new'
+  get 'restaurants/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 # Restaurant: index, create(new), show(with review)
 # Review: create(new)
